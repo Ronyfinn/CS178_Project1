2d73cb46fd45a7d50e1e6776afc32009d954fc80
Rony <ronyfinney@gmail.com>
2025-04-16 20:40:32 -0500

    added add_log

--- flaskapp.py
+++ flaskapp.py
@@ -1,4 +1,5 @@
 import boto3
+import pymysql
 from flask import Flask
 from flask import render_template
 from flask import Flask, render_template, request, redirect, url_for, flash, session
@@ -89,6 +90,48 @@ def delete_user():
 def index():
     source = get_list_of_source()
     return render_template("index.html", results=source)'''
+
+@app.route('/add-log', methods=['GET', 'POST'])
+def add_log():
+    if request.method == 'POST':
+        date = request.form['date']
+        time = request.form['time']
+        caffeine_mg = request.form['caffeine']
+        serving_size = request.form['serving_size']
+        effect = request.form['effect']
+        taste = request.form['taste']
+        context = request.form['context']
+        brand = request.form['brand']
+        flavor = request.form['flavor']
+        cost_per_serving = request.form['cost_per_serving']
+        cost_per_100mg = request.form['cost_per_100mg']
+        email = session['email']
+
+        email = session.get('email')
+        if not email:
+            flash('You must be logged in to add a log.', 'danger')
+            return redirect(url_for('login'))
+
+        query = """
+            INSERT INTO caffeine_source (brand, flavor, cost_per_serving, cost_per_100mg)
+            VALUES (%s, %s, %s, %s)
+        """
+        execute_query(query, (brand, flavor, cost_per_serving, cost_per_100mg))
+
+        query = """
+            INSERT INTO caffeine_log (user_email, source_id, date, time, caffeine_mg, serving_size, effect, taste, context)
+            VALUES (%s, LAST_INSERT_ID(), %s, %s, %s, %s, %s, %s, %s)
+        """
+        execute_query(query, (
+            email, date, time, caffeine_mg, serving_size, effect, taste, context
+        ))
+
+        flash('Caffeine log added successfully!', 'success')
+        return redirect(url_for('home'))
+
+    return render_template('add_log.html')
+
+
 
 @app.route('/display-logs')
 def display_user_logs():
